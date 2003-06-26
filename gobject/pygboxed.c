8dd59919825bb1d8586941dd6f48b917d957551a
James Henstridge <james@daa.com.au>
2003-06-26 15:06:05 +0000

    add copyright notices, and update to LGPL 2.1.
    2003-06-26  James Henstridge  <james@daa.com.au>
    
        * (lots of stuff): add copyright notices, and update to LGPL 2.1.

--- gobject/pygboxed.c
+++ gobject/pygboxed.c
@@ -1,4 +1,25 @@
-/* -*- mode: C; c-basic-indent: 4 -*- */
+/* -*- Mode: C; c-basic-offset: 4 -*-
+ * pygtk- Python bindings for the GTK toolkit.
+ * Copyright (C) 1998-2003  James Henstridge
+ *
+ *   pygboxed.c: wrapper for GBoxed and GPointer types.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
 #endif
