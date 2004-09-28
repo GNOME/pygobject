66ebbb785442fd411a251ac262763a47252cf272
Johan Dahlin <johan@src.gnome.org>
2004-09-28 09:31:44 +0000

    Forgot to commit this yesterday

---
+++ tests/test_thread.py
@@ -0,0 +1,24 @@
+import unittest
+
+from common import gobject, gtk, testhelper
+
+gobject.threads_init()
+
+class TestThread(unittest.TestCase):
+    def from_thread_cb(self, test, enum):
+        assert test == self.obj
+        assert int(enum) == 0
+        assert type(enum) != int
+        
+    def idle_cb(self):
+        self.obj = testhelper.get_test_thread()
+        self.obj.connect('from-thread', self.from_thread_cb)
+        self.obj.emit('emit-signal')
+
+    def testExtensionModule(self):
+        gtk.idle_add(self.idle_cb)
+        gtk.timeout_add(50, self.timeout_cb)
+        gtk.main()
+
+    def timeout_cb(self):
+        gtk.main_quit()
