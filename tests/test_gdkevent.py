98379feee0369bda004cfd5c0fc5decd9b05ff78
Gustavo J. A. M. Carneiro <gjc@src.gnome.org>
2005-05-02 23:38:49 +0000

    Bug 302622: Assignment to gtk.gdk.Event.window is buggy

---
+++ tests/test_gdkevent.py
@@ -0,0 +1,25 @@
+# -*- Mode: Python -*-
+
+import gc
+import unittest
+import sys
+
+from common import gtk
+
+class TestGdkEvent(unittest.TestCase):
+    def testWindowSetter(self):
+        event = gtk.gdk.Event(gtk.gdk.BUTTON_PRESS)
+
+        win1 = gtk.Window()
+        win1.realize()
+        event.window = win1.window
+        self.assertEqual(event.window, win1.window)
+        
+        win2 = gtk.Window()
+        win2.realize()
+        event.window = win2.window
+        self.assertEqual(event.window, win2.window)
+
+
+if __name__ == '__main__':
+    unittest.main()
