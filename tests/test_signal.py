b06e98fe21c4748d79d9eb966ddb301dc7779c4b
Johan Dahlin <johan@src.gnome.org>
2005-07-10 23:07:02 +0000

    Impl. Patch by Gustavo, fixes #155679
    	* gtk/gtkwidget.override (_wrap_gtk_widget_set_set_scroll_adjustments_signal)
    	(_wrap_gtk_widget_set_set_scroll_adjustments_signal): Impl.
    	Patch by Gustavo, fixes #155679
    
    	* tests/test_signal.py (TestOldStyleOverride):
    	Add unittests. Lazy Gustavo.

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -3,7 +3,7 @@
 import gc
 import unittest
 
-from common import gobject
+from common import gobject, gtk
 
 class C(gobject.GObject):
     __gsignals__ = { 'my_signal': (gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE,
@@ -15,7 +15,7 @@ class D(C):
     def do_my_signal(self, arg2):
         self.arg2 = arg2
 	C.do_my_signal(self, arg2)
-
+    
 class TestChaining(unittest.TestCase):
     def setUp(self):
         self.inst = C()
@@ -73,5 +73,39 @@ class TestGPropertyError(unittest.TestCase):
         self.assertRaises(TypeError, foo)
         gc.collect()
 
+
+class DrawingArea(gtk.DrawingArea):
+    __gsignals__ = { 'my-activate': (gobject.SIGNAL_RUN_FIRST,
+                                     gobject.TYPE_NONE, ()) ,
+                     'my-adjust': (gobject.SIGNAL_RUN_FIRST,
+                                     gobject.TYPE_NONE,
+                                   (gtk.Adjustment, gtk.Adjustment)) }
+    def __init__(self):
+        gtk.DrawingArea.__init__(self)
+        self.activated = False
+        self.adjusted = False
+        
+    def do_my_activate(self):
+        self.activated = True
+        
+    def do_my_adjust(self, hadj, vadj):
+        self.adjusted = True
+        
+DrawingArea.set_activate_signal('my-activate')
+DrawingArea.set_set_scroll_adjustments_signal('my-adjust')
+
+class TestOldStyleOverride(unittest.TestCase):
+    def testActivate(self):
+        b = DrawingArea()
+        self.assertEqual(b.activated, False)
+        b.activate()
+        self.assertEqual(b.activated, True)
+
+    def testSetScrollAdjustment(self):
+        b = DrawingArea()
+        self.assertEqual(b.adjusted, False)
+        b.set_scroll_adjustments(gtk.Adjustment(), gtk.Adjustment())
+        self.assertEqual(b.adjusted, True)
+        
 if __name__ == '__main__':
     unittest.main()
