fd15b73ff0d5902c79465776244ca5436860a9ac
Kjartan Maraas <kmaraas@gnome.org>
2006-12-14 16:08:32 +0000

    Make this work with automake 1.10 too.
    2006-12-14  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* autogen.sh: Make this work with automake 1.10 too.

--- autogen.sh
+++ autogen.sh
@@ -297,11 +297,12 @@ AUTOHEADER=`echo $AUTOCONF | sed s/autoconf/autoheader/`
 
 case $REQUIRED_AUTOMAKE_VERSION in
     1.4*) automake_progs="automake-1.4" ;;
-    1.5*) automake_progs="automake-1.5 automake-1.6 automake-1.7 automake-1.8 automake-1.9" ;;
-    1.6*) automake_progs="automake-1.6 automake-1.7 automake-1.8 automake-1.9" ;;
-    1.7*) automake_progs="automake-1.7 automake-1.8 automake-1.9" ;;
-    1.8*) automake_progs="automake-1.8 automake-1.9" ;;
-    1.9*) automake_progs="automake-1.9" ;;
+    1.5*) automake_progs="automake-1.5 automake-1.6 automake-1.7 automake-1.8 automake-1.9 automake-1.10" ;;
+    1.6*) automake_progs="automake-1.6 automake-1.7 automake-1.8 automake-1.9 automake-1.10" ;;
+    1.7*) automake_progs="automake-1.7 automake-1.8 automake-1.9 automake-1.10" ;;
+    1.8*) automake_progs="automake-1.8 automake-1.9 automake-1.10" ;;
+    1.9*) automake_progs="automake-1.9 automake-1.10" ;;
+    1.10*) automake_progs="automake-1.10" ;;
 esac
 version_check automake AUTOMAKE "$automake_progs" $REQUIRED_AUTOMAKE_VERSION \
     "http://ftp.gnu.org/pub/gnu/automake/automake-$REQUIRED_AUTOMAKE_VERSION.tar.gz" || DIE=1
