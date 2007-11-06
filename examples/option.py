1b8078ba1c1c027a8c7f3b0e1a32dd03c6e26857
Johan Dahlin <johan@src.gnome.org>
2007-11-06 08:54:35 +0000

    remove trailing whitespace
    svn path=/trunk/; revision=716

--- examples/option.py
+++ examples/option.py
@@ -5,18 +5,18 @@ from gobject.option import OptionGroup, OptionParser, make_option
 
 group = OptionGroup("example", "OptionGroup Example", "Shows all example options",
     option_list = [
-        make_option("--example", 
-                    action="store_true", 
-                    dest="example", 
+        make_option("--example",
+                    action="store_true",
+                    dest="example",
                     help="An example option."),
     ])
 
-parser = OptionParser("NAMES ...", 
+parser = OptionParser("NAMES ...",
     description="A simple gobject.option example.",
     option_list = [
-        make_option("--file", "-f", 
-                    type="filename", 
-                    action="store", 
+        make_option("--file", "-f",
+                    type="filename",
+                    action="store",
                     dest="file",
                     help="A filename option"),
         # ...
