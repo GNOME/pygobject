7aa55c4b6458f0eb051ba7154e84eb974f8b0f20
Johan Dahlin <johan@src.gnome.org>
2008-07-18 22:44:45 +0000

    improve example: show error message and allow sys.argv[1] to be specified
    svn path=/trunk/; revision=833

--- examples/gio/directory-async.py
+++ examples/gio/directory-async.py
@@ -1,17 +1,27 @@
+import sys
+
 import gobject
 import gio
 
 def next_files_done(enumerator, result):
-    print 'done!'
     for file_info in enumerator.next_files_finish(result):
         print file_info.get_name()
     loop.quit()
 
 def enumerate_children_done(gfile, result):
-    enumerator = gfile.enumerate_children_finish(result)
+    try:
+        enumerator = gfile.enumerate_children_finish(result)
+    except gobject.GError, e:
+        print 'ERROR:', e
+        loop.quit()
+        return
     enumerator.next_files_async(10, next_files_done)
 
-gfile = gio.File("/")
+if len(sys.argv) >= 2:
+    uri = sys.argv[1]
+else:
+    uri = "/"
+gfile = gio.File(uri)
 gfile.enumerate_children_async(
     "standard::name", enumerate_children_done)
 
