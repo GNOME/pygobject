f605811afe8c91f121e89b6f9ec28c70b62f4110
Gian Mario Tagliaretti <gianmt@gnome.org>
2009-06-01 22:40:56 +0200

    Wrap gio.FileInputStream.query_async
    Add the wrapper for gio.FileInputStream.query_async including docs and
    a test.

--- tests/test_gio.py
+++ tests/test_gio.py
@@ -984,3 +984,30 @@ class TestVolume(unittest.TestCase):
                 for id in ids:
                     if id is not None:
                         self.failUnless(isinstance(id, str))
+
+class TestFileInputStream(unittest.TestCase):
+    def setUp(self):
+        self._f = open("file.txt", "w+")
+        self._f.write("testing")
+        self._f.seek(0)
+        self.file = gio.File("file.txt")
+
+    def tearDown(self):
+        self._f.close()
+        if os.path.exists('file.txt'):
+            os.unlink("file.txt")
+
+    def testQueryInfoAsync(self):
+        def callback(stream, result):
+            try:
+                info = stream.query_info_finish(result)
+                self.failUnless(isinstance(info, gio.FileInfo))
+                self.failUnless(info.get_attribute_uint64("standard::size"), 7)
+            finally:
+                loop.quit()
+
+        inputstream = self.file.read()
+        inputstream.query_info_async("standard", callback)
+
+        loop = glib.MainLoop()
+        loop.run()
