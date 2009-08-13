c888b5ca722fcad6a03de585606c677c2969ebd6
Paolo Borelli <pborelli@gnome.org>
2009-08-13 21:32:07 +0200

    Allow to use automake 1.11

--- autogen.sh
+++ autogen.sh
@@ -298,12 +298,13 @@ AUTOHEADER=`echo $AUTOCONF | sed s/autoconf/autoheader/`
 
 case $REQUIRED_AUTOMAKE_VERSION in
     1.4*) automake_progs="automake-1.4" ;;
-    1.5*) automake_progs="automake-1.5 automake-1.6 automake-1.7 automake-1.8 automake-1.9 automake-1.10" ;;
-    1.6*) automake_progs="automake-1.6 automake-1.7 automake-1.8 automake-1.9 automake-1.10" ;;
-    1.7*) automake_progs="automake-1.7 automake-1.8 automake-1.9 automake-1.10" ;;
-    1.8*) automake_progs="automake-1.8 automake-1.9 automake-1.10" ;;
-    1.9*) automake_progs="automake-1.9 automake-1.10" ;;
-    1.10*) automake_progs="automake-1.10" ;;
+    1.5*) automake_progs="automake-1.5 automake-1.6 automake-1.7 automake-1.8 automake-1.9 automake-1.10 automake-1.11" ;;
+    1.6*) automake_progs="automake-1.6 automake-1.7 automake-1.8 automake-1.9 automake-1.10 automake-1.11" ;;
+    1.7*) automake_progs="automake-1.7 automake-1.8 automake-1.9 automake-1.10 automake-1.11" ;;
+    1.8*) automake_progs="automake-1.8 automake-1.9 automake-1.10 automake-1.11" ;;
+    1.9*) automake_progs="automake-1.9 automake-1.10 automake-1.11" ;;
+    1.10*) automake_progs="automake-1.10 automake-1.11" ;;
+    1.11*) automake_progs="automake-1.11" ;;
 esac
 version_check automake AUTOMAKE "$automake_progs" $REQUIRED_AUTOMAKE_VERSION \
     "http://ftp.gnu.org/pub/gnu/automake/automake-$REQUIRED_AUTOMAKE_VERSION.tar.gz" || DIE=1
