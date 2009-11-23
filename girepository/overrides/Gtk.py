5c010fe673d9bd01c27c8d7d312064665275888c
Simon van der Linden <svdlinden@src.gnome.org>
2009-11-23 22:39:12 +0100

    Remove the girepository module

--- girepository/overrides/Gtk.py
+++
@@ -1,8 +0,0 @@
-from bank.module import DynamicModule
-
-class GtkModule(DynamicModule):
-    def created(self):
-        self.init_check(0, None)
-        #if self.init_check(len(sys.argv), sys.argv):
-        #    raise RuntimeError("could not open display")
-
