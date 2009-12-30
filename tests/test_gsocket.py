771a7c3fdef7b2e98e509293a8376a81c1282286
Gian Mario Tagliaretti <gianmt@gnome.org>
2009-12-30 17:20:35 +0100

    Wrap gio.Socket.condition_wait() and add a test

--- tests/test_gsocket.py
+++ tests/test_gsocket.py
@@ -12,10 +12,14 @@ class TestSocket(unittest.TestCase):
                                gio.SOCKET_TYPE_STREAM,
                                gio.SOCKET_PROTOCOL_TCP)
 
-    def test_socket_condition(self):
+    def test_socket_condition_check(self):
         check = self.sock.condition_check(glib.IO_OUT)
         self.failUnless(isinstance(check, gobject.GFlags))
         self.failUnlessEqual(check, glib.IO_OUT | glib.IO_HUP)
+
+    def test_socket_condition_wait(self):
+        res = self.sock.condition_wait(glib.IO_OUT)
+        self.failUnless(res)
 
     def tearDown(self):
         self.sock.close()
