2357bca8d14539894b6bd0acfdc18d30b4bb4db6
Ignacio Casal Quinteiro <icq@gnome.org>
2010-07-05 16:11:07 +0200

    Do not override GdkRectangle.
    This class was lately removed from gtk+ 3, so there is no need to override
    it anymore.

--- gi/overrides/Gdk.py
+++ gi/overrides/Gdk.py
@@ -26,25 +26,6 @@ Gdk = modules['Gdk']
 
 __all__ = []
 
-class Rectangle(Gdk.Rectangle):
-
-    def __init__(self, x, y, width, height):
-        Gdk.Rectangle.__init__(self)
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-
-    def __new__(cls, *args, **kwargs):
-        return Gdk.Rectangle.__new__(cls)
-
-    def __repr__(self):
-        return '<Gdk.Rectangle(x=%d, y=%d, width=%d, height=%d)>' % (
-		    self.x, self.y, self.width, self.height)
-
-Rectangle = override(Rectangle)
-__all__.append('Rectangle')
-
 class Color(Gdk.Color):
 
     def __init__(self, red, green, blue):
