82f4cb5ebf5d992493b7a2f74cfd5f175e19eb76
Tomeu Vizoso <tomeu.vizoso@collabora.co.uk>
2010-07-29 12:34:19 +0200

    Add some more transformations to pygi-convert.sh

--- pygi-convert.sh
+++ pygi-convert.sh
@@ -8,9 +8,10 @@ for f in $FILES_TO_CONVERT; do
     -pe "s/gconf\./GConf\./g;" \
     -pe "s/GConf\.client_get_default/GConf.Client.get_default/g;" \
     -pe "s/GConf\.CLIENT_/GConf.ClientPreloadType./g;" \
+    -pe "s/GConf\.VALUE_/GConf.ValueType./g;" \
     -pe "s/gconf_client.notify_add\('\/desktop\/sugar\/collaboration\/publish_gadget',/return;gconf_client.notify_add\('\/desktop\/sugar\/collaboration\/publish_gadget',/g;" \
 \
-    -pe "s/import gtk\n/from gi.repository import Gtk\n/g;" \
+    -pe "s/import gtk\n/from gi.repository import Gtk; Gtk.require_version('2.0')\n/g;" \
     -pe "s/gtk\./Gtk\./g;" \
     -pe "s/Gtk.SIZE_GROUP_/Gtk.SizeGroupMode./g;" \
     -pe "s/Gtk.POLICY_/Gtk.PolicyType./g;" \
@@ -22,6 +23,14 @@ for f in $FILES_TO_CONVERT; do
     -pe "s/Gtk.SELECTION_/Gtk.SelectionMode./g;" \
     -pe "s/Gtk.CELL_RENDERER_MODE_/Gtk.CellRendererMode./g;" \
     -pe "s/Gtk.TREE_VIEW_COLUMN_/Gtk.TreeViewColumnSizing./g;" \
+    -pe "s/Gtk.TEXT_DIR_/Gtk.TextDirection./g;" \
+    -pe "s/Gtk.POS_/Gtk.PositionType./g;" \
+    -pe "s/Gtk.SHADOW_/Gtk.ShadowType./g;" \
+    -pe "s/Gtk.BUTTONBOX_/Gtk.ButtonBoxStyle./g;" \
+    -pe "s/Gtk.SHRINK/Gtk.AttachOptions.SHRINK/g;" \
+    -pe "s/Gtk.FILL/Gtk.AttachOptions.FILL/g;" \
+    -pe "s/Gtk.JUSTIFY_/Gtk.Justification./g;" \
+    -pe "s/Gtk.RESPONSE_/Gtk.ResponseType./g;" \
     -pe "s/Gtk.CORNER_/Gtk.CornerType./g;" \
     -pe "s/Gtk.settings_get_default/Gtk.Settings.get_default/g;" \
     -pe "s/Gtk.icon_theme_get_default/Gtk.IconTheme.get_default/g;" \
@@ -29,6 +38,7 @@ for f in $FILES_TO_CONVERT; do
     -pe "s/self.drag_source_unset\(\)/Gtk.drag_source_unset\(self\)/g;" \
     -pe "s/self.drag_dest_unset\(\)/Gtk.drag_dest_unset\(self\)/g;" \
     -pe "s/Gtk.ListStore\(([^\)]*)\)/Gtk.ListStore.newv\(\[\1\]\)/g;" \
+    -pe "s/Gtk.Alignment\(/Gtk.Alignment.new\(/g;" \
     -pe "s/self._model.filter_new\(\)/Gtk.TreeModelFilter.new\(self._model, None\)/g;" \
     -pe "#s/Gtk.ScrolledWindow\(\)/Gtk.ScrolledWindow\(None, None\)/g;" \
     -pe "#s/Gtk.Window.__init__\(self\)/Gtk.Window.__init__\(Gtk.WindowType.TOPLEVEL\)/g;" \
@@ -45,20 +55,23 @@ for f in $FILES_TO_CONVERT; do
     -pe "s/Gtk.AccelLabel\(([^,\)]+)\)/Gtk.AccelLabel\(label=\1\)/g;" \
     -pe "s/len\(self._content.get_children\(\)\) > 0/self._content.get_children\(\)/g;" \
     -pe "s/len\(self.menu.get_children\(\)\) > 0/self.menu.get_children\(\)/g;" \
-\
+    -pe "s/([^\.^ ]*)\.drag_dest_set\(/Gtk.drag_dest_set\(\1, /g;" \
     -pe "s/Gtk\..*\.__init__/gobject.GObject.__init__/g;" \
 \
+    -pe "s/Gtk.gdk.x11_/GdkX11\./g;" \
     -pe "s/Gtk.gdk\./Gdk\./g;" \
     -pe "s/Gdk.screen_width/Gdk.Screen.width/g;" \
     -pe "s/Gdk.screen_height/Gdk.Screen.height/g;" \
     -pe "s/Gdk.screen_get_default/Gdk.Screen.get_default/g;" \
+    -pe "s/Gdk.display_get_default/Gdk.Display.get_default/g;" \
+    -pe "s/screen_, x_, y_, modmask = display.get_pointer\(\)/x_, y_, modmask = display.get_pointer\(None\)/g;" \
     -pe "s/Gdk.WINDOW_TYPE_HINT_/Gdk.WindowTypeHint./g;" \
-    -pe "s/Gdk\.Rectangle/Gdk.rectangle_new/g;" \
-    -pe "s/Gdk.BUTTON_PRESS_MASK/Gdk.EventMask.BUTTON_PRESS_MASK/g;" \
-    -pe "s/Gdk.POINTER_MOTION_HINT_MASK/Gdk.EventMask.POINTER_MOTION_HINT_MASK/g;" \
-    -pe "s/Gdk.VISIBILITY_NOTIFY_MASK/Gdk.EventMask.VISIBILITY_NOTIFY_MASK/g;" \
-    -pe "s/Gdk.Color\(/Gdk.color_new\(/g;" \
+    -pe "s/Gdk.MOD1_MASK/Gdk.ModifierType.MOD1_MASK/g;" \
+    -pe "s/Gdk.([A-Z_0-9]*)_MASK/Gdk.EventMask.\1_MASK/g;" \
+    -pe "s/Gdk.VISIBILITY_FULLY_OBSCURED/Gdk.VisibilityState.FULLY_OBSCURED/g;" \
     -pe "s/Gdk.BUTTON_PRESS/Gdk.EventType.BUTTON_PRESS/g;" \
+    -pe "s/intersection = child_rect.intersect/intersects_, intersection = child_rect.intersect/g;" \
+    -pe "s/event.state/event.get_state\(\)/g;" \
 \
     -pe "s/import pango\n/from gi.repository import Pango\n/g;" \
     -pe "s/pango\./Pango\./g;" \
@@ -68,9 +81,12 @@ for f in $FILES_TO_CONVERT; do
     -pe "s/import hippo\n/from gi.repository import Hippo\n/g;" \
     -pe "s/hippo\./Hippo\./g;" \
     -pe "s/Hippo\..*\.__init__/gobject.GObject.__init__/g;" \
+    -pe "s/Hippo.PACK_/Hippo.PackFlags./g;" \
+    -pe "s/Hippo.ORIENTATION_/Hippo.Orientation./g;" \
     -pe "#s/insert_sorted\(([^,\)]*), ([^,\)]*), ([^,\)]*)\)/insert_sorted\(\1, \2, \3, None\)/g;" \
     -pe "s/self\._box\.insert_sorted/#self\._box\.insert_sorted/g;" \
     -pe "s/self._box.append\(([^,\)]*)\)/self._box.append\(\1, 0\)/g;" \
+    -pe "s/self.append\(self._buddy_icon\)/self.append\(self._buddy_icon, 0\)/g;" \
     -pe "s/self._box.sort\(([^,\)]*)\)/self._box.sort\(\1, None\)/g;" \
 \
     -pe "s/import wnck\n/from gi.repository import Wnck\n/g;" \
@@ -124,7 +140,12 @@ done
 
 NEED_GDK=`grep -R -l Gdk\. $FILES_TO_CONVERT | xargs grep -nL import\ Gdk`
 for f in $NEED_GDK; do
-    sed -i "/import Gtk/ i\from gi.repository import Gdk" $f
+    sed -i "/import Gtk/ i\from gi.repository import Gdk; Gdk.require_version('2.0')" $f
+done
+
+NEED_GDK_X11=`grep -R -l GdkX11\. $FILES_TO_CONVERT | xargs grep -nL import\ GdkX11`
+for f in $NEED_GDK_X11; do
+    sed -i "/Gdk.require_version('2.0')/ i\from gi.repository import GdkX11; GdkX11.require_version('2.0')" $f
 done
 
 NEED_SUGAR_EXT=`grep -R -l SugarExt\. $FILES_TO_CONVERT | xargs grep -nL import\ SugarExt`
