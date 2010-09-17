4ed100f3183c6325dd04461484e877bb7d4131b1
John (J5) Palmieri <johnp@redhat.com>
2010-09-17 12:08:09 -0400

    add checks so we can compile under python 3 by setting PYTHON=python3
    * compile for python 3
    * disables gio if compiling under python 3.x
    * runs only pertinant tests
    
    https://bugzilla.gnome.org/show_bug.cgi?id=615872

--- tests/runtests.py
+++ tests/runtests.py
@@ -9,6 +9,10 @@ import unittest
 # Load tests.
 if 'TEST_NAMES' in os.environ:
 	names = os.environ['TEST_NAMES'].split()
+elif 'TEST_FILES' in os.environ:
+	names = []
+	for filename in os.environ['TEST_FILES'].split():
+		names.append(filename[:-3])
 else:
 	names = []
 	for filename in glob.iglob("test_*.py"):
