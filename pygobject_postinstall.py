8cb3f2e78161639c568110aad6a807dcf59f3ae8
Dieter Verfaillie <dieterv@optionexplicit.be>
2010-11-03 09:35:52 +0100

    pygobject_postinstall.py: remove pygobject-2.0.pc treatment from postinstall as pkg-config on windows figures out the correct prefix at runtime
    Signed-off-by: Dieter Verfaillie <dieterv@optionexplicit.be>

--- pygobject_postinstall.py
+++ pygobject_postinstall.py
@@ -1,29 +1,9 @@
 # -*- coding: utf-8 -*-
 
-"""pygobject is now installed on your machine.
 
-Local configuration files were successfully updated."""
+import sys
 
-import os, re, sys
-
-pkgconfig_file = os.path.normpath(
-    os.path.join(sys.prefix,
-                 'lib/pkgconfig/pygobject-2.0.pc'))
-
-prefix_pattern=re.compile("^prefix=.*")
-version_pattern=re.compile("Version: ([0-9]+\.[0-9]+\.[0-9]+)")
-
-def replace_prefix(s):
-    if prefix_pattern.match(s):
-        s='prefix='+sys.prefix.replace("\\","/")+'\n'
-    s=s.replace("@DATADIR@",
-                os.path.join(sys.prefix,'share').replace("\\","/"))
-    
-    return s
 
 if len(sys.argv) == 2:
-    if sys.argv[1] == "-install":
-        # fixup the pkgconfig file
-        lines=open(pkgconfig_file).readlines()
-        open(pkgconfig_file, 'w').writelines(map(replace_prefix,lines))
-        print __doc__
+    if sys.argv[1] == '-install':
+        print ('pygobject is now installed on your machine.\n')
