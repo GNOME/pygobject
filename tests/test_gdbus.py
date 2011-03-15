115c4750f071270b648218c9678931a65a4b3e3d
Martin Pitt <martin.pitt@ubuntu.com>
2011-03-15 10:22:39 +0100

    [gi] Do not require signature for D-BUS methods without arguments
    Calling methods on DBusProxy objects usually requires specifying the signature
    as first argument. However, if the D-BUS method does not take any arguments,
    specifying the empty '()' signature does not give any additional information,
    so allow the caller to just call the proxy method without any arguments.
    
    Also ensure that passing a non-string signature raises a comprehensible
    exception, instead of crashing deep in the GVariant leaf constructor.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=644260

--- tests/test_gdbus.py
+++ tests/test_gdbus.py
@@ -142,6 +142,13 @@ class TestGDBusClient(unittest.TestCase):
 
             self.assertTrue('Timeout' in str(e), str(e))
 
+    def test_python_calls_sync_noargs(self):
+        # methods without arguments don't need an explicit signature
+        result = self.dbus_proxy.ListNames()
+        self.assertTrue(isinstance(result, list))
+        self.assertTrue(len(result) > 1)
+        self.assertTrue('org.freedesktop.DBus' in result)
+
     def test_python_calls_sync_errors(self):
         # error case: invalid argument types
         try:
@@ -151,6 +158,12 @@ class TestGDBusClient(unittest.TestCase):
             etype, e = sys.exc_info()[:2]
 
             self.assertTrue('InvalidArgs' in str(e), str(e))
+
+        try:
+            self.dbus_proxy.GetConnectionUnixProcessID(None, 'foo')
+            self.fail('call with None signature should raise an exception')
+        except TypeError as e:
+            self.assertTrue('signature' in str(e), str(e))
 
     def test_python_calls_async(self):
         def call_done(obj, result, user_data):
