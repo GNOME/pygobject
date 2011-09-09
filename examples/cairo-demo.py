45b0fcff9e948c65a3903c32a3957802034c5e47
Dieter Verfaillie <dieterv@optionexplicit.be>
2011-09-09 16:50:25 +0200

    Examples: fix cairo-demo.py imports

--- examples/cairo-demo.py
+++ examples/cairo-demo.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-"""Based on cairo-demo/X11/cairo-demo.c
 """
-import pygtk
-pygtk.require('2.0')
+Based on cairo-demo/X11/cairo-demo.c
+"""
+
 import cairo
 from gi.repository import Gdk, Gtk
 
