4b16427714b850e33c6020d8de1833bae19a3b87
Martin Pitt <martinpitt@gnome.org>
2012-10-28 13:44:23 +0100

    test_subprocess: Cover more child_watch_add() cases
    Verify that priority is set correctly, and also test calling without userdata.

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -8,18 +8,32 @@ from gi.repository import GLib
 
 class TestProcess(unittest.TestCase):
 
-    def _child_watch_cb(self, pid, condition, data):
-        self.data = data
-        self.loop.quit()
+    def test_child_watch_no_data(self):
+        def cb(pid, condition):
+            self.loop.quit()
 
-    def test_child_watch(self):
+        self.loop = GLib.MainLoop()
+        argv = [sys.executable, '-c', 'import sys']
+        pid, stdin, stdout, stderr = GLib.spawn_async(
+            argv, flags=GLib.SpawnFlags.DO_NOT_REAP_CHILD)
+        pid.close()
+        GLib.child_watch_add(pid, cb)
+        self.loop.run()
+
+    def test_child_watch_data_priority(self):
+        def cb(pid, condition, data):
+            self.data = data
+            self.loop.quit()
+
         self.data = None
         self.loop = GLib.MainLoop()
         argv = [sys.executable, '-c', 'import sys']
         pid, stdin, stdout, stderr = GLib.spawn_async(
             argv, flags=GLib.SpawnFlags.DO_NOT_REAP_CHILD)
         pid.close()
-        GLib.child_watch_add(pid, self._child_watch_cb, 12345)
+        id = GLib.child_watch_add(pid, cb, 12345, GLib.PRIORITY_HIGH)
+        self.assertEqual(self.loop.get_context().find_source_by_id(id).priority,
+                         GLib.PRIORITY_HIGH)
         self.loop.run()
         self.assertEqual(self.data, 12345)
 
