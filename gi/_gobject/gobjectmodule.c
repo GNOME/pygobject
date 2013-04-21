0dff1940caf52ea5f1de27cc801ea6d4dab3a446
Jose Rostagno <joserostagno@vijona.com.ar>
2013-04-21 19:09:59 -0300

    Remove g_type_init conditional call
    It's deprecated in the glib version we depend on.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=698763

--- gi/_gobject/gobjectmodule.c
+++ gi/_gobject/gobjectmodule.c
@@ -2166,9 +2166,6 @@ PYGLIB_MODULE_START(_gobject, "_gobject")
 {
     PyObject *d;
 
-#if !defined(GLIB_VERSION_2_36)
-    g_type_init();
-#endif
     pyglib_init();
 
     d = PyModule_GetDict(module);
