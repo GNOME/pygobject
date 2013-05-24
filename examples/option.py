991b0b0d581aa7bd1ea069c3938dfc903bb7c799
Martin Pitt <martinpitt@gnome.org>
2013-05-24 13:03:07 +0200

    examples/option.py: Port to GI and Python 3

--- examples/option.py
+++ examples/option.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python
-# gnome-python/pygobject/examples/option.py
 
-from gobject.option import OptionGroup, OptionParser, make_option
+from gi.repository import GLib
 
-group = OptionGroup("example", "OptionGroup Example", "Shows all example options",
-                    option_list=[make_option("--example",
-                                             action="store_true",
-                                             dest="example",
-                                             help="An example option."),
-                                ])
+group = GLib.option.OptionGroup(
+    "example", "OptionGroup Example", "Shows all example options",
+    option_list=[GLib.option.make_option("--example",
+                                         action="store_true",
+                                         dest="example",
+                                         help="An example option."),
+                ])
 
-parser = OptionParser("NAMES ...",
-                      description="A simple gobject.option example.",
-                      option_list=[make_option("--file", "-f",
-                                               type="filename",
-                                               action="store",
-                                               dest="file",
-                                               help="A filename option"),
-                                   # ...
-                                  ])
+parser = GLib.option.OptionParser(
+    "NAMES ...", description="A simple gobject.option example.",
+    option_list=[GLib.option.make_option("--file", "-f",
+                                         type="filename",
+                                         action="store",
+                                         dest="file",
+                                         help="A filename option"),
+                 # ...
+                ])
 
 parser.add_option_group(group)
 
 parser.parse_args()
 
-print "group: example ", group.values.example
-print "parser: file", parser.values.file
+print("group: example " + str(group.values.example))
+print("parser: file " + str(parser.values.file))
