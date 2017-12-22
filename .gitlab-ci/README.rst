adf5e51ccf9d9dbf938c0da496e0d4d81d3284f6
Christoph Reiter <reiter.christoph@gmail.com>
2017-12-22 16:34:53 +0100

    Add gitlab CI tests
    Adds a Dockerfile which is available on the docker hub
    and gets used by the gitlab test runner.
    
    The tests get executed in an Ubuntu 17.04 image for various Python
    versions and consist of building, testing, code quality testing and
    documentation build testing.

---
+++ .gitlab-ci/README.rst
@@ -0,0 +1,1 @@
+The Dockerfile is available at https://hub.docker.com/r/lazka/pygobject/
---
+++ .gitlab-ci/test-docker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+virtualenv --python="${PYTHON}" /tmp/venv
+source /tmp/venv/bin/activate
+
+python -m pip install git+https://github.com/pygobject/pycairo.git
+python -m pip install pyflakes pycodestyle
+
+export PKG_CONFIG_PATH=/tmp/venv/share/pkgconfig
+export MALLOC_CHECK_=3
+export MALLOC_PERTURB_=$((${RANDOM} % 255 + 1))
+PYVER=$(python -c "import sys; sys.stdout.write(str(sys.version_info[0]))")
+
+# BUILD
+./autogen.sh --with-python=python
+make
+
+# TESTS
+xvfb-run -a make check
+
+# CODE QUALITY CHECKS
+make check.quality
+
+# DOCUMENTATION CHECKS
+if [[ "${PYVER}" == "2" ]]; then
+    python -m pip install sphinx sphinx_rtd_theme
+    python -m sphinx -W -a -E -b html -n docs docs/_build
+fi;
