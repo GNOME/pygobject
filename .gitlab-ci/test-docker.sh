e56975aaf9bb976397b5961b7b34adcdd91b5a94
Christoph Reiter <reiter.christoph@gmail.com>
2018-02-06 23:04:18 +0100

    gitlab-ci: adjust PKG_CONFIG_PATH for pycairo
    pycairo git now installs things into $prefix/lib/pkgconfig

--- .gitlab-ci/test-docker.sh
+++ .gitlab-ci/test-docker.sh
@@ -8,7 +8,7 @@ source /tmp/venv/bin/activate
 python -m pip install git+https://github.com/pygobject/pycairo.git
 python -m pip install flake8
 
-export PKG_CONFIG_PATH=/tmp/venv/share/pkgconfig
+export PKG_CONFIG_PATH=/tmp/venv/lib/pkgconfig
 export MALLOC_CHECK_=3
 export MALLOC_PERTURB_=$((${RANDOM} % 255 + 1))
 PYVER=$(python -c "import sys; sys.stdout.write(str(sys.version_info[0]))")
