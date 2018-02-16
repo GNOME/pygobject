9c3cb6b49c33f53d637e80aad68a67e89d0eb944
Christoph Reiter <reiter.christoph@gmail.com>
2018-02-16 10:12:42 +0100

    gitlab-ci: use ccache

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sudo docker build --build-arg HOST_USER_ID="$UID" --tag "pygobject" \
+TAG="lazka/pygobject:pyenv"
+
+sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
 sudo docker run -e PYENV_VERSION='3.6.4' --rm \
     --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
-    --tty --interactive "pygobject" bash
+    --tty --interactive "${TAG}" bash
