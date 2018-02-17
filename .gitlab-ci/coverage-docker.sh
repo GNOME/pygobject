e798af4dc95c2781368389bf5a271d4927ceab54
Christoph Reiter <reiter.christoph@gmail.com>
2018-02-17 15:23:49 +0100

    gitlab-ci: Add coverage reports

---
+++ .gitlab-ci/coverage-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+python -m pip install coverage
+
+python -m coverage combine _coverage
+python -m coverage html -d _coverage/report-python
+rm -f _coverage/.coverage*
+genhtml --ignore-errors=source --rc lcov_branch_coverage=1 _coverage/*.lcov -o _coverage/report-c
+rm -f _coverage/*.lcov
