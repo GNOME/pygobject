1534289ce5934b73b5c980f8e246192009337776
Christoph Reiter <reiter.christoph@gmail.com>
2018-02-20 16:38:25 +0100

    gitlab-ci: Add MSYS2 test jobs
    This assumes a gitlab runner with a "win32" tag which has
    MSYS2 installed in the default path and uses the "cmd" shell.

---
+++ .gitlab-ci/test-msys2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+export PATH="/c/msys64/$MSYSTEM/bin:$PATH"
+if [[ "$MSYSTEM" == "MINGW32" ]]; then
+    export MSYS2_ARCH="i686"
+else
+    export MSYS2_ARCH="x86_64"
+fi
+
+pacman --noconfirm -Suy
+
+pacman --noconfirm -S --needed \
+    base-devel \
+    mingw-w64-$MSYS2_ARCH-toolchain \
+    mingw-w64-$MSYS2_ARCH-$PYTHON-cairo \
+    mingw-w64-$MSYS2_ARCH-$PYTHON \
+    mingw-w64-$MSYS2_ARCH-$PYTHON-pip \
+    mingw-w64-$MSYS2_ARCH-$PYTHON-pytest \
+    mingw-w64-$MSYS2_ARCH-gobject-introspection \
+    mingw-w64-$MSYS2_ARCH-libffi \
+    mingw-w64-$MSYS2_ARCH-glib2 \
+    mingw-w64-$MSYS2_ARCH-gtk3 \
+    git \
+    autoconf-archive
+
+$PYTHON setup.py test
