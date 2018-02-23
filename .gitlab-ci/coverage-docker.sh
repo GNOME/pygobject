79ee93ea667c7a2cdacf9c770c0e60dc624ced0b
Christoph Reiter <reiter.christoph@gmail.com>
2018-02-23 18:15:55 +0100

    coverage: exclude external headers. See #168
    While we could just pass --no-external we want the coverage of
    the gobject-introspection files.

--- .gitlab-ci/coverage-docker.sh
+++ .gitlab-ci/coverage-docker.sh
@@ -7,6 +7,12 @@ python -m pip install coverage
 # Make the Windows paths match our current layout
 python ./.gitlab-ci/fixup-cov-paths.py coverage/.coverage* coverage/*.lcov
 
+# Remove external headers (except gi tests)
+for path in coverage/*.lcov; do
+    lcov --rc lcov_branch_coverage=1 -r "${path}" '/usr/include/*' -o "${path}"
+    lcov --rc lcov_branch_coverage=1 -r "${path}" '/home/*' -o "${path}"
+done
+
 python -m coverage combine coverage
 python -m coverage html --ignore-errors -d coverage/report-python
 genhtml --ignore-errors=source --rc lcov_branch_coverage=1 \
