25fa14dc1bffa7489af741f40403442912d5d533
Christoph Reiter <reiter.christoph@gmail.com>
2018-04-01 01:13:09 +0200

    gitlab-ci: update pyenv versions and add pypy3 job

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-TAG="lazka/pygobject:pyenv"
+TAG="lazka/pygobject:v3"
 
 sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
-sudo docker run -e PYENV_VERSION='3.6.4' --rm \
+sudo docker run -e PYENV_VERSION='3.6.5' --rm \
     --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
     --tty --interactive "${TAG}" bash
