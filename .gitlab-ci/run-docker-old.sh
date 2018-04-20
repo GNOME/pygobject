a80873e2a041f6e060c82467e96a1f3dee6eb4f4
Christoph Reiter <reiter.christoph@gmail.com>
2018-04-20 20:31:29 +0200

    docker script: make them work with active selinux (fedora)

--- .gitlab-ci/run-docker-old.sh
+++ .gitlab-ci/run-docker-old.sh
@@ -6,6 +6,6 @@ TAG="registry.gitlab.gnome.org/gnome/pygobject/old:v2"
 
 sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile.old" .
-sudo docker run --rm \
+sudo docker run --rm --security-opt label=disable \
     --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
     --tty --interactive "${TAG}" bash
--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -6,6 +6,6 @@ TAG="registry.gitlab.gnome.org/gnome/pygobject/main:v4"
 
 sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
-sudo docker run -e PYENV_VERSION='3.6.5' --rm \
+sudo docker run -e PYENV_VERSION='3.6.5' --rm --security-opt label=disable \
     --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
     --tty --interactive "${TAG}" bash
