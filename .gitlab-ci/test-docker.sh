34805800df9d4468de9dcf0fefc1f45702fcfcb5
Christoph Reiter <reiter.christoph@gmail.com>
2018-05-03 10:58:09 +0200

    ci: fix doc build not being run
    We check the exact version number but that has changed with the last update.
    Check the major/minor + impl instead.

--- .gitlab-ci/test-docker.sh
+++ .gitlab-ci/test-docker.sh
@@ -4,14 +4,14 @@ set -e
 
 python --version
 
-PYVER=$(python -c "import sys; sys.stdout.write(''.join(map(str, sys.version_info[:3])))")
+PYVER=$(python -c "import sys; sys.stdout.write('.'.join(map(str, sys.version_info[:2])))")
 PYIMPL=$(python -c "import sys, platform; sys.stdout.write(platform.python_implementation())")
 SOURCE_DIR="$(pwd)"
 COV_DIR="${SOURCE_DIR}/coverage"
 export MALLOC_CHECK_=3
 export MALLOC_PERTURB_=$((${RANDOM} % 255 + 1))
 export G_SLICE="debug-blocks"
-export COVERAGE_FILE="${COV_DIR}/.coverage.${PYVER}"
+export COVERAGE_FILE="${COV_DIR}/.coverage.${CI_JOB_NAME}"
 export CCACHE_BASEDIR="$(pwd)"
 export CCACHE_DIR="${CCACHE_BASEDIR}/_ccache"
 
@@ -37,7 +37,7 @@ fi;
 python -m flake8
 
 # DOCUMENTATION CHECKS
-if [[ "${PYENV_VERSION}" == "2.7.14" ]]; then
+if [[ "${PYVER}" == "2.7" ]] && [[ "${PYIMPL}" == "CPython" ]]; then
     python -m pip install sphinx sphinx_rtd_theme
     python -m sphinx -W -a -E -b html -n docs docs/_build
 fi;
@@ -48,4 +48,4 @@ xvfb-run -a python -m coverage run tests/runtests.py
 
 # COLLECT GCOV COVERAGE
 lcov --rc lcov_branch_coverage=1 --directory . --capture --output-file \
-    "${COV_DIR}/${PYVER}.lcov"
+    "${COV_DIR}/${CI_JOB_NAME}.lcov"
