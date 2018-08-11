2937a07f4e5d28aa3d1aeff6adc9d74d68108782
Christoph Reiter <reiter.christoph@gmail.com>
2018-08-11 17:49:59 +0200

    ci: try lcov master for collection coverage on MSYS2
    msys2 has updated to gcc8 and lcov can't read the resulting files.
    Maybe this helps.

--- .gitlab-ci/test-msys2.sh
+++ .gitlab-ci/test-msys2.sh
@@ -48,10 +48,10 @@ export COVERAGE_FILE="${COV_DIR}/.coverage.${COV_KEY}"
 $PYTHON setup.py build_tests
 MSYSTEM= $PYTHON -m coverage run tests/runtests.py
 
-curl -O -J -L "https://github.com/linux-test-project/lcov/releases/download/v1.13/lcov-1.13.tar.gz"
-tar -xvzf lcov-1.13.tar.gz
+curl -O -J -L "https://github.com/linux-test-project/lcov/archive/master.tar.gz"
+tar -xvzf lcov-master.tar.gz
 
-./lcov-1.13/bin/lcov \
+./lcov-master/bin/lcov \
     --rc lcov_branch_coverage=1 --no-external \
     --directory . --capture --output-file \
     "${COV_DIR}/${COV_KEY}.lcov"
