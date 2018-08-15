3583a25b3344c791a2b1ae314f0bc5d7f203c25f
Christoph Reiter <reiter.christoph@gmail.com>
2018-08-15 17:10:39 +0200

    tests: silence some deprecation warnings due to ValueArray usage

--- tests/test_gobject.py
+++ tests/test_gobject.py
@@ -714,13 +714,16 @@ class TestGValue(unittest.TestCase):
         self.assertEqual(testhelper.value_array_get_nth_type(value, 1), GObject.TYPE_STRING)
 
     def test_value_array_append_gvalue(self):
-        arr = GObject.ValueArray.new(0)
-        arr.append(GObject.Value(GObject.TYPE_UINT, 0xffffffff))
-        arr.append(GObject.Value(GObject.TYPE_STRING, 'foo_bar'))
-        self.assertEqual(arr.get_nth(0), 0xffffffff)
-        self.assertEqual(arr.get_nth(1), 'foo_bar')
-        self.assertEqual(testhelper.value_array_get_nth_type(arr, 0), GObject.TYPE_UINT)
-        self.assertEqual(testhelper.value_array_get_nth_type(arr, 1), GObject.TYPE_STRING)
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', DeprecationWarning)
+
+            arr = GObject.ValueArray.new(0)
+            arr.append(GObject.Value(GObject.TYPE_UINT, 0xffffffff))
+            arr.append(GObject.Value(GObject.TYPE_STRING, 'foo_bar'))
+            self.assertEqual(arr.get_nth(0), 0xffffffff)
+            self.assertEqual(arr.get_nth(1), 'foo_bar')
+            self.assertEqual(testhelper.value_array_get_nth_type(arr, 0), GObject.TYPE_UINT)
+            self.assertEqual(testhelper.value_array_get_nth_type(arr, 1), GObject.TYPE_STRING)
 
     def test_gerror_boxing(self):
         error = GLib.Error('test message', domain='mydomain', code=42)
