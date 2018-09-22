85e292c04e479ecfc12f025ffe47daf5fc184f69
Christoph Reiter <reiter.christoph@gmail.com>
2018-09-22 22:03:27 +0200

    ci: add a gtk4 job. Fixes #249
    Let it fail for now

---
+++ .gitlab-ci/run-docker-gtk4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+TAG="registry.gitlab.gnome.org/gnome/pygobject/gtk4:v1"
+
+sudo docker build --tag "${TAG}" --file "Dockerfile.gtk4" .
+sudo docker run --rm --security-opt label=disable \
+    --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
+    --tty --interactive "${TAG}" bash
