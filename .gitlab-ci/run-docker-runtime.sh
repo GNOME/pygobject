3f0fff6bdce67636d33730ed7b9214fff19e74e7
Christoph Reiter <reiter.christoph@gmail.com>
2019-04-07 10:05:20 +0200

    CI: add a helper script for running gnome-runtime-images in docker

---
+++ .gitlab-ci/run-docker-runtime.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+TAG="registry.gitlab.gnome.org/gnome/gnome-runtime-images/gnome:master"
+
+sudo docker pull "${TAG}"
+sudo docker run --privileged --rm --security-opt label=disable \
+    --volume "$(pwd)/..:/home/user/app" --workdir "/home/user/app" \
+    --tty --interactive "${TAG}" xvfb-run -a flatpak run --filesystem=host \
+    --share=network --socket=x11 --command=bash org.gnome.Sdk//master
