6376c11c0f93e35a222d29d654651c1b4cf0c384
Christoph Reiter <reiter.christoph@gmail.com>
2019-05-17 16:06:05 +0200

    ci: work around pip3 install --user not working in recent fdo-sdk
    https://gitlab.com/freedesktop-sdk/freedesktop-sdk/issues/776

--- .gitlab-ci/test-flatpak.sh
+++ .gitlab-ci/test-flatpak.sh
@@ -3,4 +3,7 @@
 set -e
 
 python3 -m pip install --user pytest pytest-faulthandler
+# for some reason pip3 fails the first time now..
+# https://gitlab.com/freedesktop-sdk/freedesktop-sdk/issues/776
+python3 -m pip install --user pytest pytest-faulthandler
 python3 setup.py test -s
