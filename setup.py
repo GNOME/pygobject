adeb2d8001043a03360e959463ad45704c5d5ae7
Christoph Reiter <reiter.christoph@gmail.com>
2019-06-16 16:17:50 +0200

    version bump

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ from distutils import dir_util, log
 from distutils.spawn import find_executable
 
 
-PYGOBJECT_VERSION = "3.30.5"
+PYGOBJECT_VERSION = "3.30.6"
 GLIB_VERSION_REQUIRED = "2.38.0"
 GI_VERSION_REQUIRED = "1.46.0"
 PYCAIRO_VERSION_REQUIRED = "1.11.1"
