ac3a69cb03bd526bd8f059681302f8802e9511a4
Christoph Reiter <reiter.christoph@gmail.com>
2020-02-15 07:26:21 +0100

    CI: update pip in the xenial job
    Some packages have started failing to install with the pip shipped
    with xenial.

--- .gitlab-ci/test-docker-old.sh
+++ .gitlab-ci/test-docker-old.sh
@@ -16,6 +16,7 @@ mkdir -p "${COV_DIR}"
 mkdir -p "${CCACHE_DIR}"
 
 # test
+python -m pip install --upgrade pip
 python -m pip install pycairo pytest pytest-faulthandler coverage
 python setup.py build_tests
 xvfb-run -a python -m coverage run --context "${COV_KEY}" tests/runtests.py
