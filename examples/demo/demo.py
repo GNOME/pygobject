7df001655da6bd31c023c010a66e272f468a762a
Christoph Reiter <reiter.christoph@gmail.com>
2020-11-30 18:31:22 +0100

    examples/demo: Use require_version to force gtk3
    The examples don't work with gtk4 so set the proper version

--- examples/demo/demo.py
+++ examples/demo/demo.py
@@ -25,6 +25,13 @@ import os
 import sys
 import textwrap
 
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Pango', '1.0')
+gi.require_version('PangoCairo', '1.0')
+gi.require_version('GdkPixbuf', '2.0')
+gi.require_version('GtkSource', '4')
+
 from gi.repository import GLib, GObject, Pango, GdkPixbuf, Gtk, Gio
 
 try:
