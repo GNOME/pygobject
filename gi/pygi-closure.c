4b421597d1885c725cbcdd44c4d04277454bee85
Christoph Reiter <reiter.christoph@gmail.com>
2022-08-08 09:26:44 +0200

    Revert "pygi: Fix girepository deprecation warnings"
    This reverts commit fc776c2058e11da5c3a4cebeea7f313057bc079f.
    
    This might have caused a regression, see #535.
    Until we know more revert this for now.

--- gi/pygi-closure.c
+++ gi/pygi-closure.c
@@ -24,10 +24,6 @@
 #include "pygi-ccallback.h"
 #include "pygi-info.h"
 
-#ifndef GI_CHECK_VERSION
-#define GI_CHECK_VERSION(x,y,z) 0
-#endif
-
 extern PyObject *_PyGIDefaultArgPlaceholder;
 
 typedef struct _PyGICallbackCache
@@ -636,13 +632,8 @@ end:
 
 void _pygi_invoke_closure_free (PyGICClosure* invoke_closure)
 {
-#if GI_CHECK_VERSION(1, 72, 0)
-    g_callable_info_destroy_closure (invoke_closure->info,
-                                     invoke_closure->closure);
-#else
     g_callable_info_free_closure (invoke_closure->info,
                                   invoke_closure->closure);
-#endif
 
     if (invoke_closure->info)
         g_base_info_unref ( (GIBaseInfo*) invoke_closure->info);
@@ -680,13 +671,8 @@ _pygi_make_native_closure (GICallableInfo* info,
     Py_XINCREF (closure->user_data);
 
     fficlosure =
-#if GI_CHECK_VERSION(1, 72, 0)
-        g_callable_info_create_closure (info, &closure->cif, _pygi_closure_handle,
-                                        closure);
-#else
         g_callable_info_prepare_closure (info, &closure->cif, _pygi_closure_handle,
                                          closure);
-#endif
     closure->closure = fficlosure;
 
     /* Give the closure the information it needs to determine when
