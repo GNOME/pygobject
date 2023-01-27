6152280fcc209c37632e1e6d78f58b7c4c9fb4bb
Christoph Reiter <reiter.christoph@gmail.com>
2023-01-27 20:01:48 +0100

    CI: try to make the flatpak tests run again
    Half a year ago they broke due to some changes of either the
    gnome-runtime-images docker images, or the contained flatpak, or
    something else: https://gitlab.gnome.org/GNOME/gnome-runtime-images/-/issues/7
    
    This creates a fake dbus system bus so flatpak run doesn't fail.
    
    After this it turned out that pip install is now completely broken
    in the sdk and it installs packages to a place where it can't find them
    afterwards, so just use a venv instead.
    
    Finally the pygobject test suite still crashes at some point,
    but at least it's better then before.

--- .gitlab-ci/test-flatpak-gtk4.sh
+++ .gitlab-ci/test-flatpak-gtk4.sh
@@ -8,7 +8,9 @@ export COVERAGE_FILE="${COV_DIR}/.coverage.${COV_KEY}"
 mkdir -p "${COV_DIR}"
 
 export TEST_GTK_VERSION=4.0
-python3 -m pip install --user pytest pytest-faulthandler coverage
+python3 -m venv _venv
+. _venv/bin/activate
+python3 -m pip install pytest pytest-faulthandler coverage
 python3 setup.py build_tests
 python3 -m coverage run --context "${COV_KEY}" tests/runtests.py
 python3 -m coverage lcov -o "${COV_DIR}/${COV_KEY}.py.lcov"
--- .gitlab-ci/test-flatpak.sh
+++ .gitlab-ci/test-flatpak.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-python3 -m pip install --user pytest pytest-faulthandler
-# for some reason pip3 fails the first time now..
-# https://gitlab.com/freedesktop-sdk/freedesktop-sdk/issues/776
-python3 -m pip install --user pytest pytest-faulthandler
+python3 -m venv _venv
+. _venv/bin/activate
+python3 -m pip install pytest pytest-faulthandler
 python3 setup.py test -s
