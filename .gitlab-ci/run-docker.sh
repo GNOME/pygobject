f9cc69ae949553ce78a1e0e508300fd48804a6e4
Christoph Reiter <reiter.christoph@gmail.com>
2023-11-04 21:36:32 +0100

    Update CI env
    Mainly 3.12.0b2 -> 3.12.0
    And pypy3.8 -> pypy3.9 because pypy3.8 is no longer supported

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-TAG="registry.gitlab.gnome.org/gnome/pygobject/main:v21"
+TAG="registry.gitlab.gnome.org/gnome/pygobject/main:v22"
 
 sudo docker build --build-arg HOST_USER_ID="$UID" --tag "${TAG}" \
     --file "Dockerfile" .
