cfecf8061296ad74207f7a675f6f5722584c44a6
Arjan Molenaar <gaphor@gmail.com>
2024-02-20 11:46:42 +0100

    Update sdist script to use the version defined by meson

--- .gitlab-ci/build-sdists.sh
+++ .gitlab-ci/build-sdists.sh
@@ -13,23 +13,16 @@ rm -Rf _sdist_build
 meson setup _sdist_build
 meson dist --no-tests --allow-dirty -C _sdist_build
 mv _sdist_build/meson-dist/*.tar.xz dist/
+
+VERSION=$(meson introspect --projectinfo --indent _sdist_build | awk -F'"' '/version/ { print $4 }')
+
 rm -Rf _sdist_build
 
 # Now the sdist for pypi
 # Mark odd versions as PEP440 development versions (e.g. 0.1.0 -> 0.1.0.dev0)
-cp pyproject.toml pyproject.toml.bak
-awk '{
-        if (/^version = "/) {
-            split($0, a, "[ .\"]+");
-            if (a[4] % 2 != 0)
-                printf "version = \"%s.%s.%s.dev0\"\n", a[3], a[4], a[5];
-            else
-                print $0;
-        } else {
-            print $0;
-        }
-    }' pyproject.toml > _temp && mv _temp pyproject.toml
+meson rewrite kwargs set project / version ${VERSION}.dev0
 
 python -m pip install --upgrade build
 python -m build --sdist
-mv pyproject.toml.bak pyproject.toml
+
+meson rewrite kwargs set project / version ${VERSION}
