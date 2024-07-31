GNOME/pygobject

commit bf80d0f34af5062fd1201d71fbcd3a34bf1d1a80

override connection.register_object to prevent an invocation object from leaking

gi/overrides/Gio.py
@@ -90,6 +90,34 @@ __all__.append('DBusArgInfo')
 
 
 @override
+class DBusConnection(Gio.DBusConnection):
+    __init__ = _warn_init(Gio.DBusConnection)
+
+    def register_object(self,
+                        object_path,
+                        interface_info,
+                        method_call_closure,
+                        get_property_closure,
+                        set_property_closure):
+        def wrapped_method_call_closure(connection, sender, object_path,
+                                        interface_name, method_name, parameters,
+                                        invocation):
+            method_call_closure(connection, sender, object_path,
+                                interface_name, method_name, parameters,
+                                invocation)
+            invocation._unref()
+
+        return super().register_object(object_path,
+                                       interface_info,
+                                       wrapped_method_call_closure,
+                                       get_property_closure,
+                                       set_property_closure)
+
+
+__all__.append('DBusConnection')
+
+
+@override
 class DBusMethodInfo(Gio.DBusMethodInfo):
     __init__ = _warn_init(Gio.DBusMethodInfo)
 

commit e5ae70821c11894a6ec9ca95e3763709765ea9c4

Make certain Gio.DBusConnection.register_object arguments optional
Otherwise we will break apps that omit these arguments. Looking
through search results on GitHub, the number seems non-trivial.

method_call_closure should be optional in any case, according to
the docs.

Regression in bf80d0f34af5062fd1201d71fbcd3a34bf1d1a80

gi/overrides/Gio.py
@@ -96,16 +96,19 @@ class DBusConnection(Gio.DBusConnection):
     def register_object(self,
                         object_path,
                         interface_info,
-                        method_call_closure,
-                        get_property_closure,
-                        set_property_closure):
-        def wrapped_method_call_closure(connection, sender, object_path,
-                                        interface_name, method_name, parameters,
-                                        invocation):
-            method_call_closure(connection, sender, object_path,
-                                interface_name, method_name, parameters,
-                                invocation)
-            invocation._unref()
+                        method_call_closure=None,
+                        get_property_closure=None,
+                        set_property_closure=None):
+        if method_call_closure is not None:
+            def wrapped_method_call_closure(connection, sender, object_path,
+                                            interface_name, method_name, parameters,
+                                            invocation):
+                method_call_closure(connection, sender, object_path,
+                                    interface_name, method_name, parameters,
+                                    invocation)
+                invocation._unref()
+        else:
+            wrapped_method_call_closure = None
 
         return super().register_object(object_path,
                                        interface_info,

tests/test_gdbus.py
@@ -258,6 +258,34 @@ class TestGDBusClient(unittest.TestCase):
 class TestDBusConnection:
 
     @unittest.skipUnless(has_dbus, "no dbus running")
+    def test_register_object(self):
+        object_path = "/pygobject/Test"
+        interface_xml = """
+            <node>
+                <interface name='org.pygobject.Test'>
+                    <method name='test' />
+                </interface>
+            </node>"""
+        interface_info = Gio.DBusNodeInfo.new_for_xml(interface_xml).interfaces[0]
+        bus = Gio.bus_get_sync(Gio.BusType.SESSION)
+
+        for args in (
+            (object_path, interface_info),
+            (object_path, interface_info, None, None, None)
+        ):
+            reg_id = bus.register_object(*args)
+            bus.unregister_object(reg_id)
+
+        for kwargs in (
+            {"object_path": object_path, "interface_info": interface_info},
+            {"object_path": object_path, "interface_info": interface_info,
+             "method_call_closure": None, "get_property_closure": None,
+             "set_property_closure": None},
+        ):
+            reg_id = bus.register_object(**kwargs)
+            bus.unregister_object(reg_id)
+
+    @unittest.skipUnless(has_dbus, "no dbus running")
     def test_connection_invocation_ref_count(self):
         """Invocation object should not leak a reference."""
         invocation, errors = self.run_server(self.client_call)