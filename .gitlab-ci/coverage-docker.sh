72d64f6b5a97cb7a801d3aae5182977000bb6e9d
Arjan Molenaar <gaphor@gmail.com>
2024-08-05 22:32:07 +0200

    Clean up coverage report
    Remove cairo and meson-private files.

--- .gitlab-ci/coverage-docker.sh
+++ .gitlab-ci/coverage-docker.sh
@@ -9,7 +9,9 @@ python ./.gitlab-ci/fixup-lcov-paths.py coverage/*.lcov
 for path in coverage/*.lcov; do
     lcov --config-file .gitlab-ci/lcovrc -r "${path}" '/usr/include/*' -o "${path}"
     lcov --config-file .gitlab-ci/lcovrc -r "${path}" '/home/*' -o "${path}"
+    lcov --config-file .gitlab-ci/lcovrc -r "${path}" '*/meson-private/*' -o "${path}"
     lcov --config-file .gitlab-ci/lcovrc -r "${path}" '*/msys64/*' -o "${path}"
+    lcov --config-file .gitlab-ci/lcovrc -r "${path}" '*site-packages/*' -o "${path}"
     lcov --config-file .gitlab-ci/lcovrc -r "${path}" '*subprojects/*' -o "${path}"
     lcov --config-file .gitlab-ci/lcovrc -r "${path}" '*tmp-introspect*' -o "${path}"
 done
