908c88b46bf775c0b6576dfeb48b33c49ae7c1d8
Arjan Molenaar <gaphor@gmail.com>
2025-04-19 21:33:59 +0200

    Update build to resemble main build

--- .gitlab-ci/build-sdists.sh
+++ .gitlab-ci/build-sdists.sh
@@ -2,27 +2,19 @@
 
 set -e
 
-rm -Rf _venv_sdist
-python3 -m venv _venv_sdist
-source _venv_sdist/bin/activate
-mkdir -p dist
+python -m pip install --upgrade pdm
+python -m pdm build
 
-# First we create a tarball for GNOME
-python -m pip install --upgrade meson pycairo
-rm -Rf _sdist_build
-meson setup _sdist_build
-meson dist --no-tests --allow-dirty -C _sdist_build
-mv _sdist_build/meson-dist/*.tar.xz dist/
+VERSION=$(grep version meson.build | head -1 | sed "s/^.*'\([0-9\.]*\)'.*$/\1/")
 
-VERSION=$(meson introspect --projectinfo --indent _sdist_build | awk -F'"' '/version/ { print $4 }')
+if [[ "$VERSION" =~ ^[0-9]\.[0-9]*[13579]\. ]]
+then
+cat << EOF
 
-rm -Rf _sdist_build
+****************** ATTENTION ******************
+          This is an UNstable release.
+       Do NOT upload this release to PyPI.
+****************** ATTENTION ******************
 
-# Now the sdist for pypi
-# Mark odd versions as PEP440 development versions (e.g. 0.1.0 -> 0.1.0.dev0)
-[[ "$VERSION" =~ ^[0-9]\.[0-9]*[13579]\. ]] && meson rewrite kwargs set project / version ${VERSION}.dev0
-
-python -m pip install --upgrade build
-python -m build --sdist
-
-meson rewrite kwargs set project / version ${VERSION}
+EOF
+fi
