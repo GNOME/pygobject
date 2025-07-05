e7dface967b8fc8498558bb32c0927b0f7dded05
Arjan Molenaar <gaphor@gmail.com>
2025-07-05 14:14:24 +0200

    backport: Test in a flatpak build
    pixbuf loaders are sandboxed, so we need to test with a build command,
    to ensure the sandbox is disabled.
    
    See also: https://gitlab.gnome.org/GNOME/gnome-build-meta/-/issues/986

--- .gitlab-ci/test-flatpak.sh
+++ .gitlab-ci/test-flatpak.sh
@@ -2,23 +2,8 @@
 
 set -e
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
+sed -i "/TEST_GTK_VERSION:/s/'.*'/'${TEST_GTK_VERSION:-3.0}'/" .gitlab-ci/org.gnome.PyGObject.Devel.yaml
+xvfb-run -a flatpak-builder --user --keep-build-dirs --verbose --disable-rofiles-fuse flatpak_ci .gitlab-ci/org.gnome.PyGObject.Devel.yaml
 
-if [[ "$1" == "inflatpak" ]]; then
-    COV_DIR="$(pwd)/coverage"
-    COV_KEY="flatpak-$TEST_GTK_VERSION"
-    export COVERAGE_FILE="${COV_DIR}/.coverage.${COV_KEY}"
-    mkdir -p "${COV_DIR}"
-
-    python3 --version
-
-    pip install pycairo meson meson-python pytest pytest-cov
-    pip install --config-settings=setup-args="-Dtests=true" --no-build-isolation --editable .
-    python -m pytest -v --cov
-    python3 -m coverage lcov -o "${COV_DIR}/${COV_KEY}.py.lcov"
-    chmod -R 777 "${COV_DIR}"
-else
-    # https://gitlab.gnome.org/GNOME/gnome-runtime-images/-/issues/7
-    export DBUS_SYSTEM_BUS_ADDRESS="$(dbus-daemon --session --print-address --fork)"
-    xvfb-run -a flatpak run --user --filesystem=host --share=network --socket=x11 --command=bash org.gnome.Sdk//master -x "${DIR}"/test-flatpak.sh inflatpak
-fi
+mv .flatpak-builder/build/pygobject/coverage .
+chmod -R 777 coverage
