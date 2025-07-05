4f10ee7a52bd9cd44030fa39bd9d3d4b2377df9f
Arjan Molenaar <gaphor@gmail.com>
2025-07-05 14:39:06 +0200

    debug dbus error

--- .gitlab-ci/test-flatpak.sh
+++ .gitlab-ci/test-flatpak.sh
@@ -3,6 +3,6 @@
 set -e
 
 # https://gitlab.gnome.org/GNOME/gnome-runtime-images/-/issues/7
-DBUS_SYSTEM_BUS_ADDRESS="$(dbus-daemon --session --print-address --fork)"
-export DBUS_SYSTEM_BUS_ADDRESS
+# DBUS_SYSTEM_BUS_ADDRESS="$(dbus-daemon --session --print-address --fork)"
+# export DBUS_SYSTEM_BUS_ADDRESS
 xvfb-run -a flatpak-builder --user --verbose --disable-rofiles-fuse flatpak_ci org.gnome.PyGObject.Devel.json
