e10a4dc9320945f1d15503fae9d1b6be96af745b
Arjan Molenaar <gaphor@gmail.com>
2025-08-11 08:30:26 +0200

    CallableCache now keeps a reference to the unerlying GI info

--- gi/pygi-cache.c
+++ gi/pygi-cache.c
@@ -666,6 +666,7 @@ _callable_cache_generate_args_cache_real (PyGICallableCache *callable_cache,
 static void
 _callable_cache_deinit_real (PyGICallableCache *cache)
 {
+    g_clear_pointer (&cache->info, gi_base_info_unref);
     g_clear_pointer (&cache->to_py_args, g_slist_free);
     g_clear_pointer (&cache->arg_name_hash, g_hash_table_unref);
     g_clear_pointer (&cache->args_cache, g_ptr_array_unref);
@@ -680,6 +681,8 @@ _callable_cache_init (PyGICallableCache *cache,
 {
     gint n_args;
     GIBaseInfo *container;
+
+    cache->info = gi_base_info_ref (GI_BASE_INFO (callable_info));
 
     if (cache->deinit == NULL)
         cache->deinit = _callable_cache_deinit_real;
@@ -1103,7 +1106,6 @@ _vfunc_cache_invoke_real (PyGIFunctionCache *function_cache,
                           size_t py_nargsf,
                           PyObject *py_kwnames)
 {
-    PyGIVFuncCache *vfunc_cache = (PyGIVFuncCache *) function_cache;
     Py_ssize_t nargs = PyVectorcall_NARGS (py_nargsf);
     PyObject *py_gtype;
     GType implementor_gtype;
@@ -1126,7 +1128,7 @@ _vfunc_cache_invoke_real (PyGIFunctionCache *function_cache,
      * class hierarchy. e.g. Object.do_func vs. SubObject.do_func might
      * retrieve a different vfunc address but GI gives us the same vfunc info.
      */
-    state->function_ptr = gi_vfunc_info_get_address ((GIVFuncInfo *) vfunc_cache->info,
+    state->function_ptr = gi_vfunc_info_get_address (function_cache->callable_cache.info,
                                                     implementor_gtype,
                                                     &error);
     if (pygi_error_check (&error)) {
@@ -1142,8 +1144,6 @@ _vfunc_cache_invoke_real (PyGIFunctionCache *function_cache,
 static void
 _vfunc_cache_deinit_real (PyGICallableCache *callable_cache)
 {
-    gi_base_info_unref (((PyGIVFuncCache *) callable_cache)->info);
-
     _function_cache_deinit_real (callable_cache);
 }
 
@@ -1171,9 +1171,6 @@ pygi_vfunc_cache_new (GICallableInfo *info)
         g_free (vfunc_cache);
         return NULL;
     }
-
-    /* Required by _vfunc_cache_invoke_real() */
-    vfunc_cache->info = gi_base_info_ref ((GIBaseInfo *) info);
 
     return function_cache;
 }
