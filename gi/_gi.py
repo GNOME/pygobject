4de7885c56d04fe08cae55684961aa5ae9f13294
Arjan Molenaar <gaphor@gmail.com>
2025-10-02 17:56:52 +0200

    Move extension modules to toplevel
    Having the (C-) extension modules on toplevel allows
    modules to be initialized with `PyImport_AppendInittab`, which would
    potentially make PyGObject usable on Android.

---
+++ gi/_gi.py
@@ -0,0 +1,11 @@
+# ruff: noqa: F401, F403
+
+from _gi import *
+from _gi import (
+    _API,
+    _PyGObject_API,
+    _gvalue_get,
+    _gvalue_get_type,
+    _gvalue_set,
+    _install_metaclass,
+)
