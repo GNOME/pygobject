24206a1f0ee556c6126a8616253b28f6c098c835
Arjan Molenaar <gaphor@gmail.com>
2025-10-04 10:14:48 +0200

    fix

--- subprojects/packagefiles/pythoncapi-compat-dist.py
+++
@@ -1,8 +0,0 @@
-# Remove the .git folder from pythoncapi-compat
-# Workaround for https://github.com/mesonbuild/meson/issues/11750
-
-import shutil
-import os
-from pathlib import Path
-
-shutil.rmtree(Path(os.environ["MESON_DIST_ROOT"]) / "subprojects" / "pythoncapi-compat" / ".git")
