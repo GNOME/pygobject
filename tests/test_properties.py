eeefeaf3ce22f66150c0741962ba57b0c284660a
Arjan Molenaar <gaphor@gmail.com>
2025-11-16 14:24:03 +0100

    Add test for unicode param spec

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -1399,6 +1399,13 @@ class CPropertiesTestBase:
         self.set_prop(obj, "gtype", str)
         self.assertEqual(self.get_prop(obj, "gtype"), GObject.TYPE_STRING)
 
+    def test_unichar(self):
+        obj = Regress.TestObj()
+        self.assertEqual(self.get_prop(obj, "unichar"), 0)
+
+        self.set_prop(obj, "unichar", ord("🙃"))
+        self.assertEqual(self.get_prop(obj, "unichar"), ord("🙃"))
+
     def test_hash_table(self):
         obj = Regress.TestObj()
         self.assertEqual(self.get_prop(obj, "hash-table"), None)
