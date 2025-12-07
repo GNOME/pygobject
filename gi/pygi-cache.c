761acee1f61a1c45e1d6725dcf78efed5f000103
Arjan Molenaar <gaphor@gmail.com>
2025-12-07 10:33:19 +0100

    One function to marshal to python with cache marshallers

--- gi/pygi-cache.c
+++ gi/pygi-cache.c
@@ -18,7 +18,6 @@
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "pygi-argument.h"
 #include "pygi-basictype.h"
 #include "pygi-closure.h"
 #include "pygi-error.h"
@@ -1055,60 +1054,4 @@ pygi_closure_cache_new (GICallableInfo *info)
     }
 
     return closure_cache;
-}
-
-PyObject *
-pygi_constant_cache_invoke (GIConstantInfo *info)
-{
-    GITypeInfo *type_info = gi_constant_info_get_type_info (info);
-    gssize py_arg_index;
-    PyGIInvokeState state = { 0 };
-    gpointer cleanup_data = NULL;
-    PyObject *object;
-    GIArgument value = PYGI_ARG_INIT;
-
-    gi_constant_info_get_value (info, &value);
-
-    PyGIArgCache *cache =
-        pygi_arg_cache_new (type_info, /*arg_info=*/NULL, GI_TRANSFER_NOTHING,
-                            PYGI_DIRECTION_TO_PYTHON,
-                            /*callable_cache=*/NULL, 0, &py_arg_index);
-
-    object = cache->to_py_marshaller (&state, /*callable_cache=*/NULL, cache,
-                                      &value, &cleanup_data);
-
-    if (cache->to_py_cleanup)
-        cache->to_py_cleanup (&state, cache, cleanup_data, value.v_pointer,
-                              TRUE);
-
-    pygi_arg_cache_free (cache);
-
-    gi_constant_info_free_value (info, &value);
-
-    return object;
-}
-
-PyObject *
-pygi_argument_to_py (GITypeInfo *type_info, GIArgument value)
-{
-    gssize py_arg_index;
-    PyGIInvokeState state = { 0 };
-    gpointer cleanup_data = NULL;
-    PyObject *object;
-
-    PyGIArgCache *cache =
-        pygi_arg_cache_new (type_info, /*arg_info=*/NULL, GI_TRANSFER_NOTHING,
-                            PYGI_DIRECTION_TO_PYTHON,
-                            /*callable_cache=*/NULL, 0, &py_arg_index);
-
-    object = cache->to_py_marshaller (&state, /*callable_cache=*/NULL, cache,
-                                      &value, &cleanup_data);
-
-    if (cache->to_py_cleanup && value.v_pointer)
-        cache->to_py_cleanup (&state, cache, cleanup_data, value.v_pointer,
-                              TRUE);
-
-    pygi_arg_cache_free (cache);
-
-    return object;
 }
