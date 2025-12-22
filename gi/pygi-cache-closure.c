0b6abd18c801d32b904579c85c34dc2a79d0ba2a
Arjan Molenaar <gaphor@gmail.com>
2025-12-22 12:42:37 +0100

    Remove all was_processed checks
    We know, because we have cleanup data.

--- gi/pygi-cache-closure.c
+++ gi/pygi-cache-closure.c
@@ -205,7 +205,7 @@ _pygi_marshal_cleanup_from_py_interface_callback (PyGIInvokeState *state,
                                                   PyGIMarshalCleanupData data,
                                                   gboolean was_processed)
 {
-    if (was_processed) pygi_marshal_cleanup_data_destroy (&data);
+    pygi_marshal_cleanup_data_destroy (&data);
 }
 
 static void
