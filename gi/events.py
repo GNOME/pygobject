GNOME/pygobject

commit 881c86105635e0c6b89ecb8c8c361630e186e62b

events: Try calling _run_forever_{setup,cleanup}
The corresponding code changed upstream in cpython, see
  https://github.com/python/cpython/issues/110771

Closes: #733

gi/events.py
@@ -164,16 +164,21 @@ class _GLibEventLoopMixin:
                     self._quit_funcs[-1]()
             return
 
-        # Outermost nesting
-        self._check_closed()
-        self._set_coroutine_origin_tracking(self._debug)
-        self._thread_id = threading.get_ident()
+        if hasattr(self, "_run_forever_setup"):
+            self._run_forever_setup()
+        else:
+            # Outermost nesting
+            self._check_closed()
+            self._set_coroutine_origin_tracking(self._debug)
+            self._thread_id = threading.get_ident()
 
-        old_agen_hooks = sys.get_asyncgen_hooks()
-        sys.set_asyncgen_hooks(
-            firstiter=self._asyncgen_firstiter_hook,
-            finalizer=self._asyncgen_finalizer_hook,
-        )
+            old_agen_hooks = sys.get_asyncgen_hooks()
+            sys.set_asyncgen_hooks(
+                firstiter=self._asyncgen_firstiter_hook,
+                finalizer=self._asyncgen_finalizer_hook,
+            )
+            asyncio._set_running_loop(self)
+
         try:
             asyncio._set_running_loop(self)
             assert not self._selector._source._dispatching
@@ -192,14 +197,19 @@ class _GLibEventLoopMixin:
             self._selector.detach()
             self._context.release()
             self._thread_id = None
-            asyncio._set_running_loop(None)
-            with contextlib.suppress(AttributeError):
-                self._set_coroutine_origin_tracking(False)
-            sys.set_asyncgen_hooks(*old_agen_hooks)
+
+            if hasattr(self, "_run_forever_setup"):
+                self._run_forever_cleanup()
+            else:
+                asyncio._set_running_loop(None)
+                with contextlib.suppress(AttributeError):
+                    self._set_coroutine_origin_tracking(False)
+                sys.set_asyncgen_hooks(*old_agen_hooks)
+
+                self._stopping = False
 
             self._quit_funcs.pop()
             assert len(self._quit_funcs) == 0
-            self._stopping = False
 
     def time(self):
         return GLib.get_monotonic_time() / 1000000

commit 8ef48233e472a6afce29af73e9939e20de73351d

events: Remove duplicate asyncio._set_running_loop call
The _run_forever_setup function already calls this and the call was
also added to the else branch. However, the further call later on is
redundant with this and should be removed.

Fixes: 881c86105635 ("events: Try calling _run_forever_{setup,cleanup}")

gi/events.py
@@ -181,7 +181,6 @@ class _GLibEventLoopMixin:
             asyncio._set_running_loop(self)
 
         try:
-            asyncio._set_running_loop(self)
             assert not self._selector._source._dispatching
             self._may_iterate = True
             self._selector.attach()