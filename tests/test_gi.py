GNOME/pygobject

commit 75fb7de1774ccb049bf0688f581e21edfe6618fa

gi: Fix caller-allocating arrays
See which kind of array we need to allocate, and only allocate a GArray
when that is the expected type. There is no support for allocating C
arrays fo now.

Related: https://gitlab.gnome.org/GNOME/pygobject/-/issues/687

Signed-off-by: Sergey Bugaev <bugaevc@gmail.com>

gi/pygi-invoke.c
@@ -346,8 +346,22 @@ _caller_alloc (PyGIArgCache *arg_cache, GIArgument *arg)
     } else if (arg_cache->type_tag == GI_TYPE_TAG_ARRAY) {
         PyGIArgGArray *array_cache = (PyGIArgGArray *)arg_cache;
 
-        arg->v_pointer =
-            g_array_new (TRUE, TRUE, (guint)array_cache->item_size);
+        switch (gi_type_info_get_array_type (arg_cache->type_info)) {
+        case GI_ARRAY_TYPE_C:
+            return FALSE;
+        case GI_ARRAY_TYPE_ARRAY:
+            arg->v_pointer =
+                g_array_new (TRUE, TRUE, (guint)array_cache->item_size);
+            break;
+        case GI_ARRAY_TYPE_PTR_ARRAY:
+            arg->v_pointer = g_ptr_array_new ();
+            break;
+        case GI_ARRAY_TYPE_BYTE_ARRAY:
+            arg->v_pointer = g_byte_array_new ();
+            break;
+        default:
+            return FALSE;
+        }
     } else {
         return FALSE;
     }

commit a257511c5ff72d96a94d83c4e4e5d0f750964549

pygi-invoke: Handle caller-allocated fixed-size C-Arrays
Since commit 75fb7de1 we have an improved handling for caller-allocated
arrays, but that did not take in consideration that C-arrays can also be
caller-allocated when they have a known fixed size.

So handle the case and test them with the marshalling tests that we
already have.

Fixes: 75fb7de1
Closes: #743
Helps: #687

gi/pygi-invoke.c
@@ -346,9 +346,16 @@ _caller_alloc (PyGIArgCache *arg_cache, GIArgument *arg)
         }
     } else if (arg_cache->type_tag == GI_TYPE_TAG_ARRAY) {
         PyGIArgGArray *array_cache = (PyGIArgGArray *)arg_cache;
+        size_t fixed_size;
 
         switch (gi_type_info_get_array_type (arg_cache->type_info)) {
         case GI_ARRAY_TYPE_C:
+            if (gi_type_info_get_array_fixed_size (arg_cache->type_info,
+                                                   &fixed_size)) {
+                arg->v_pointer =
+                    g_malloc0 (array_cache->item_size * fixed_size);
+                break;
+            }
             return FALSE;
         case GI_ARRAY_TYPE_ARRAY:
             arg->v_pointer =

tests/test_gi.py
@@ -1168,6 +1168,37 @@ class TestArray(unittest.TestCase):
             ],
         )
 
+    def test_array_fixed_caller_allocated_out(self):
+        self.assertEqual(
+            GIMarshallingTests.array_fixed_caller_allocated_out(),
+            [
+                -1,
+                0,
+                1,
+                2,
+            ],
+        )
+
+    def test_array_fixed_caller_allocated_struct_out(self):
+        array = GIMarshallingTests.array_fixed_caller_allocated_struct_out()
+        self.assertEqual(len(array), 4)
+
+        self.assertIsInstance(array[0], GIMarshallingTests.SimpleStruct)
+        self.assertEqual(array[0].long_, -2)
+        self.assertEqual(array[0].int8, -1)
+
+        self.assertIsInstance(array[1], GIMarshallingTests.SimpleStruct)
+        self.assertEqual(array[1].long_, 1)
+        self.assertEqual(array[1].int8, 2)
+
+        self.assertIsInstance(array[2], GIMarshallingTests.SimpleStruct)
+        self.assertEqual(array[2].long_, 3)
+        self.assertEqual(array[2].int8, 4)
+
+        self.assertIsInstance(array[3], GIMarshallingTests.SimpleStruct)
+        self.assertEqual(array[3].long_, 5)
+        self.assertEqual(array[3].int8, 6)
+
 
 class TestLengthArray(unittest.TestCase):
     def test_length_array_utf8_none_inout(self):