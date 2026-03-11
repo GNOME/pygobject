3db1e088383d665235665105321be0aed1b4d91e
Arjan Molenaar <gaphor@gmail.com>
2026-03-11 17:45:56 +0100

    Build pygobject with meson
    So gstreamer can find the pkgconf file

--- .gitlab-ci/regress-gstreamer.sh
+++ .gitlab-ci/regress-gstreamer.sh
@@ -8,20 +8,19 @@ set -e
 
 python --version
 
-python -m venv /tmp/venv
-# shellcheck disable=SC1091
-source /tmp/venv/bin/activate
-
-pip install meson meson-python pycairo
-pip install --no-build-isolation .
-
 sudo apt update
 sudo apt install -y flex bison libdrm-dev libgudev-1.0-dev libogg-dev \
     libopus-dev libavfilter-dev libsoup-3.0-dev libflac-dev libmp3lame-dev \
-    libvpx-dev libnice-dev libjson-glib-dev
+    libvpx-dev libnice-dev libjson-glib-dev meson ninja-build \
+    libgtest-dev libopenh264-dev
+
+meson setup -Dtests=false -Dpycairo=disabled --wipe _gi_build
+meson compile -C _gi_build
+sudo meson install -C _gi_build
 
 git clone --branch $GSTREAMER_VERSION --depth 1 https://gitlab.freedesktop.org/gstreamer/gstreamer.git
-cd gstreamer
 
-meson setup -Dpython=enabled -Dintrospection=enabled -Dugly=disabled _build
-VERBOSE=1 meson test -v -C _build --suite gst-python
+meson setup -Dpython=enabled -Dintrospection=enabled -Dugly=disabled _gst_build gstreamer
+meson compile -C _gst_build
+GST_PLUGIN_SCANNER=$(pwd)/_gst_build/subprojects/gstreamer/libs/gst/helpers/gst-plugin-scanner VERBOSE=1 meson test -v -C _gst_build --suite gst-python
+
