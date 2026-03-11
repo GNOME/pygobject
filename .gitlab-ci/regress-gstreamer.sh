87ee12a2ba41165a89deb76c4ab5439263e99c47
Arjan Molenaar <gaphor@gmail.com>
2026-03-11 16:17:03 +0100

    Regression test in venv
    Like we do all our builds.

--- .gitlab-ci/regress-gstreamer.sh
+++ .gitlab-ci/regress-gstreamer.sh
@@ -8,29 +8,20 @@ set -e
 
 python --version
 
+python -m venv /tmp/venv
+# shellcheck disable=SC1091
+source /tmp/venv/bin/activate
+
+pip install meson meson-python pycairo
+pip install --no-build-isolation .
+
 sudo apt update
 sudo apt install -y flex bison libdrm-dev libgudev-1.0-dev libogg-dev \
     libopus-dev libavfilter-dev libsoup-3.0-dev libflac-dev libmp3lame-dev \
-    libvpx-dev libnice-dev libjson-glib-dev meson ninja-build
-
+    libvpx-dev libnice-dev libjson-glib-dev
 
 git clone --branch $GSTREAMER_VERSION --depth 1 https://gitlab.freedesktop.org/gstreamer/gstreamer.git
 cd gstreamer
 
-cat > subprojects/pygobject.wrap << EOF
-[wrap-git]
-directory = pygobject
-url = https://gitlab.gnome.org/GNOME/pygobject.git
-revision = ${CI_COMMIT_SHA:-main}
-depth = 1
-
-[provide]
-pygobject-3.0 = pygobject_dep
-EOF
-
-echo "========== subprojects/pygobject.wrap =========="
-cat subprojects/pygobject.wrap
-echo "================================================"
-
 meson setup -Dpython=enabled -Dintrospection=enabled -Dugly=disabled _build
-meson test -C _build --suite gst-python
+VERBOSE=1 meson test -v -C _build --suite gst-python
